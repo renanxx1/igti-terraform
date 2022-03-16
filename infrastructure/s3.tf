403af0d5b82576a0199878d3f2fd06e4a935b2d0
renanxx1 <45301080+renanxx1@users.noreply.github.com>
2022-03-15 22:52:29 -0300

    change bucket name

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket" "dl" {
 
 
 resource "aws_s3_bucket" "stream" {
-  bucket = "igti-ney-streaming-bucket"
+  bucket = "igti-edc-streaming-bucket"
   acl    = "private"
 
   tags = {
